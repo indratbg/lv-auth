7739a57961561a59ebf652cdd257dd53f0bad20e
Indra Tobing <indra.tbg@gmail.com>
2020-04-09 21:42:36 +0700

    modif login

--- resources/js/stores/login.js
+++ resources/js/stores/login.js
@@ -1,44 +1,45 @@
-import $axios from '../api.js'
+import $axios from "../api.js";
 
-const state = () => ({
 
-})
+const state = () => ({});
 
-const mutations = {
-
-}
+const mutations = {};
 
 const actions = {
     userRegister({
         state,
         commit
     }, payload) {
-
         return new Promise((resolve, reject) => {
-            $axios.post(`/register`, payload)
-                .then((response) => {
-                    commit('SET_ERRORS', {
-                        errors: [],
-                        message: null
-                    }, {
+            $axios
+                .post(`/register`, payload)
+                .then(response => {
+                    commit(
+                        "SET_ERRORS", {
+                            errors: [],
+                            message: null
+                        }, {
+                            root: true
+                        }
+                    );
+                    commit("SET_SUCCESS", response.data, {
                         root: true
-                    })
-                    commit('SET_SUCCESS', response.data, {
-                        root: true
-                    })
-                    resolve(response)
+                    });
+                    resolve(response);
                 })
-                .catch((error) => {
-                    commit('SET_SUCCESS', {
-                        success: null
-                    }, {
-                        root: true
-                    })
+                .catch(error => {
+                    commit(
+                        "SET_SUCCESS", {
+                            success: null
+                        }, {
+                            root: true
+                        }
+                    );
                     if (error.response) {
                         if (error.response.status == 422) {
-                            commit('SET_ERRORS', error.response.data, {
+                            commit("SET_ERRORS", error.response.data, {
                                 root: true
-                            })
+                            });
                         }
                     } else if (error.request) {
                         // The request was made but no response was received
@@ -47,77 +48,76 @@ const actions = {
                         // console.log(error.request);
                     } else {
                         // Something happened in setting up the request that triggered an Error
-                        console.log('Error', error.message);
+                        // console.log("Error" + error.message);
                     }
                     // console.log(error.config);
-                    reject(error)
-                })
-        })
+                    reject(error);
+                });
+        });
     },
     userLogin({
         state,
         commit
     }, payload) {
-
-        commit('SET_LOADING', true, {
+        commit("SET_LOADING", true, {
             root: true
-        })
+        });
 
         return new Promise((resolve, reject) => {
-            $axios.post(`/login`, payload)
-                .then((response) => {
+            $axios
+                .post(`/login`, payload)
+                .then(response => {
                     if (response.status == 200) {
-                        localStorage.setItem('token', response.data.access_token)
-                        commit('SET_TOKEN', response.data.access_token, {
+                        localStorage.setItem(
+                            "token",
+                            response.data.access_token
+                        );
+                        commit("SET_TOKEN", response.data.access_token, {
                             root: true
-                        })
+                        });
                     }
-                    commit('SET_LOADING', false, {
+                    commit("SET_LOADING", false, {
                         root: true
-                    })
-                    resolve(response)
+                    });
+                    resolve(response);
                 })
-                .catch((errors) => {
-                    commit('SET_ERRORS', errors.response.data)
-                    commit('SET_LOADING', false, {
+                .catch(errors => {
+                    commit("SET_ERRORS", errors.response.data);
+                    commit("SET_LOADING", false, {
                         root: true
-                    })
-                })
-
-
-
-        })
+                    });
+                });
+        });
     },
 
     userLogout(context) {
         if (context.rootGetters.loggedIn) {
-
-            $axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token')
+            $axios.defaults.headers.common["Authorization"] =
+                "Bearer " + localStorage.getItem("token");
             return new Promise((resolve, reject) => {
-                localStorage.removeItem('token')
+                localStorage.removeItem("token");
 
-                context.commit('SET_TOKEN', null, {
+                context.commit("SET_TOKEN", null, {
                     root: true
-                })
+                });
 
-                $axios.post('/logout')
-                    .then((response) => {
-
-                        resolve(response.data)
+                $axios
+                    .post("/logout")
+                    .then(response => {
+                        resolve(response.data);
                     })
-                    .catch((error) => {
+                    .catch(error => {
                         //console.log(error.config);
-                        reject(error)
-                    })
-            })
+                        reject(error);
+                    });
+            });
         }
-    }
+    },
 
-
-}
+};
 export default {
     namespaced: true,
     state,
     actions,
     mutations
-}
+};
